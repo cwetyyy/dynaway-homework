3ab9e8d07bbd60e06bf7874a5e47824806aae003
cwetyyy <cvetina.todorova9702@abv.bg>
2021-03-21 16:10:15 +0100

    Task 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,6 +14,9 @@ export class HomePage {
 
   ionViewWillEnter(): void {
     this.assets = []
-    this.assetService.getAll().subscribe(assets => this.assets = assets)
+    this.assetService.getAll().subscribe((assets) => {
+      this.assets = assets;
+      console.log(this.assets);
+    })
   }
 }
