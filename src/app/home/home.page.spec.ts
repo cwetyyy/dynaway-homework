dc7649c754996b7d3373ccb717f9a13e8e4205e0
cwetyyy <cvetina.todorova9702@abv.bg>
2021-03-22 15:06:03 +0100

    Task 3

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 import { IonicModule } from '@ionic/angular'
-
+import { AssetService } from '../shared/services/asset.service'
+import { AssetCardComponent } from './asset-card/asset-card.component'
 import { HomePage } from './home.page'
 
 describe('HomePage', () => {
@@ -9,7 +10,8 @@ describe('HomePage', () => {
 
   beforeEach(waitForAsync(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ HomePage, AssetCardComponent ],
+      providers: [{ provide: AssetService }],
       imports: [IonicModule.forRoot()]
     }).compileComponents()
 
@@ -20,5 +22,19 @@ describe('HomePage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
-  })
+  });
+
+  it('should display assets, which are defined', () => {
+    const assets = component.assets
+    expect(assets).toBeDefined()
+  });  
+
+  it('should render ion-content',() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('ion-content').textContent).toBeTruthy;
+  });
+
+  it('should have called ionViewWillEnter function',() => {
+    expect(component.ionViewWillEnter).toHaveBeenCalled;
+  });
 })
